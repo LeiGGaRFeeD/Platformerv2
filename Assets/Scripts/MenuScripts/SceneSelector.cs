1e5e0c9035286ae7f4f5538530685f4c2efe7f63
LeiGGaRFeeD <ridefoot@gmail.com>
2023-03-10 19:41:16 +0300

    Main menu scene
    Added main menu scene
    Changed folders
    Changed version
    Actual improvments

---
+++ Assets/Scripts/MenuScripts/SceneSelector.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour
+{
+    int startedGame = PlayerPrefs.GetInt("startedGame");
+    private bool _debug = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void QuitFromGame()
+    {
+        Debug.Log("Quit from application");
+        Application.Quit();
+    }
+    public void GoToSceneSelector()
+    {
+        if (startedGame == 1)
+        {
+            Debug.Log("Go to scene selection");
+            SceneManager.LoadScene("sceneSelection");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("startedGame", 1);
+            GoToLevelOne();
+        }
+    }
+    public void GoToLevelOne()
+    {
+        Debug.Log("Go to level one");
+        SceneManager.LoadScene("LevelOne");
+    }
+    public void GoToLevelTwo()
+    {
+        Debug.Log("Go to level two");
+        SceneManager.LoadScene("LevelTwo");
+    }
+    public void GoToLevelThree()
+    {
+        Debug.Log("Go to level three");
+        SceneManager.LoadScene("LevelThree");
+    }
+    public void GoToMainMenu()
+    {
+        Debug.Log("Go to main menu");
+        SceneManager.LoadScene("MainMenu");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (_debug == false)
+        {
+            Debug.Log("Debug is ON");
+            _debug = true;
+        }
+        else
+        {
+            Debug.Log("Debug is OFF");
+            _debug = false;
+        }
+        if (_debug == true)
+        {
+            if (Input.GetKeyUp(KeyCode.R))
+            {
+                PlayerPrefs.SetInt("startedGame", 0);
+            }
+        }
+    }
+}
