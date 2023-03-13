7a262fe152c1be7bceed0fe2bc835f89f7a02c86
LeiGGaRFeeD <ridefoot@gmail.com>
2023-03-13 20:07:41 +0300

    Fill level selection scene
    Added sign with arrow
    
    Fill level selection scene
    
    Added reset button in main menu

--- Assets/Debug/Scripts/ResetAllProgress.cs
+++ Assets/Debug/Scripts/ResetAllProgress.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class ResetAllProgress : MonoBehaviour
 {
     private bool _Pdebug;
+    [SerializeField] GameObject progressResetedText;
+    [SerializeField] GameObject titleText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        progressResetedText.SetActive(false);
+        titleText.SetActive(true);
     }
 
+    public void ResetButton()
+    {
+        Debug.Log("Reset all progress");
+        PlayerPrefs.SetInt("levelOpened", 1);
+        PlayerPrefs.SetInt("money", 0);
+        PlayerPrefs.SetInt("startedGame", 0);
+        progressResetedText.SetActive(true);
+        titleText.SetActive(false);
+        InvokeRepeating("TurningOffText", 1.5f,0);
+    }
+    private void TurningOffText()
+    {
+        progressResetedText.SetActive(false);
+        titleText.SetActive(true);
+    }
     // Update is called once per frame
     void Update()
     {
